6ad15ca89ee674c97bfab2f1895a9fe1f2bdafae
Dwyane Lacey <dwaynelacey.acm@gmail.com>
2022-06-18 09:11:08 -0400

    Added class method to TreeNode to begin storing data from artist data dataset

--- Artist Recommendation.py
+++ Artist Recommendation.py
@@ -1,8 +1,10 @@
+from ArtistData import artist_data
 class TreeNode:
-    def __init__(self, genre=None, artist=None, ranking=None, albums=None):
+    def __init__(self, genre=None, artist=None, ranking=None, song=None, albums=None):
         self.genre = genre
         self.artist = artist
         self.ranking = ranking
+        self.song = song
         self.albums = albums
         self.children = {}
     
@@ -12,12 +14,12 @@ class TreeNode:
         else:
             self.children[genre] = TreeNode(genre)
     
-    def add_artist_node(self, genre, artist, ranking, albums):
+    def add_artist_node(self, genre, artist, ranking, song, albums):
         if genre not in self.children.keys():
             self.add_genre_node(genre)
         current_genre = self.children[genre]
         if artist not in current_genre.children.keys():
-            current_genre.children[artist] = TreeNode(genre, artist, ranking, albums)
+            current_genre.children[artist] = TreeNode(genre, artist, ranking, song, albums)
         else:
             print(f"The artist '{artist}' is already stored in the tree.")
 
@@ -26,28 +28,25 @@ class TreeNode:
         for key in self.children.keys():
             print(key)
 
-
+    def store_data_set(self, data_set):
+        for data in data_set:
+            self.add_artist_node(data[0], data[1], data[2], data[3], data[4])
 
 
 music_tree = TreeNode()
-music_tree.add_genre_node("Rock")
-music_tree.add_genre_node("Rock")
-music_tree.add_genre_node("Rap")
-music_tree.add_genre_node("Metal")
-music_tree.add_genre_node("Country")
-music_tree.add_genre_node("Rap")
-print("\n")
 
-music_tree.add_artist_node("Rock", "Linkin Park", "35/100", "4")
-music_tree.add_artist_node("Rock", "Five Finger Death Punch", "20/100", "6")
-music_tree.add_artist_node("Rock", "Bring Me The Horizon", "1/100", "8")
 
-for artist in music_tree.children["Rock"].children.values():
+
+
+music_tree.store_data_set(artist_data)
+
+for artist in music_tree.children["Rap"].children.values():
     print(f"""
 --------------------------------------
     Genre: {artist.genre}
     Artist: {artist.artist}
-    Billboard Ranking: {artist.ranking}
+    Billboard Ranking: {artist.ranking}/100
+    Top Song: {artist.song}
     # of Albums: {artist.albums}
 --------------------------------------
     """)
